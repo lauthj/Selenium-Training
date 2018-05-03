c5b4e51efea22c79eb449329f829cee6655ef49a
DIR\sudhindra.kembhavi <skembha@caltex.com.au>
2018-05-03 13:51:22 +1000

    Explicit wait, modified the test to use chrome browser rather than IE

--- src/test/java/com/pages/LoginPage.java
+++ src/test/java/com/pages/LoginPage.java
@@ -2,10 +2,14 @@ package com.pages;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 	WebDriver driver;
@@ -42,7 +46,12 @@ public class LoginPage {
 	{
 		driver.get(url);
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		//Implicit wait
+		//driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+		//Explicit Wait
+		WebDriverWait wait=new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.elementToBeClickable(userName));
 		
 	}
 	

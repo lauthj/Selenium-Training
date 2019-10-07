424d4576f4170ec1a6bdccad754b54209bb1888a
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2019-10-07 16:17:45 -0500

    first code commit

--- src/test/java/com/tests/CalculatorImpl.java
+++ src/test/java/com/tests/CalculatorImpl.java
@@ -1,25 +1,43 @@
 package com.tests;
 
 public class CalculatorImpl implements Calculator {
-
+	
+	static private int result;
+	
 	public void add(int x, int y) {
 		// TODO Auto-generated method stub
+		
+		CalculatorImpl.setResult(x + y);
 
 	}
 
 	public void subtract(int x, int y) {
 		// TODO Auto-generated method stub
+		
+		CalculatorImpl.setResult(x - y);
 
 	}
 
 	public void multiply(int y, int y2) {
 		// TODO Auto-generated method stub
+		
+		CalculatorImpl.setResult(y * y2);
 
 	}
 
 	public void divide(int x, int y) {
 		// TODO Auto-generated method stub
+		
+		CalculatorImpl.setResult(x / y);
 
+	}
+
+	public static int getResult() {
+		return result;
+	}
+
+	public static void setResult(int result) {
+		CalculatorImpl.result = result;
 	}
 
 }
